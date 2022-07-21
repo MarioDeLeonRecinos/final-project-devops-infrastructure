a1e1239a5b462ef62fb8a2abdbac9b4cb93af3f4
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-20 18:59:08 -0600

    fix quota

--- buildVariables/production/variable.tf
+++ buildVariables/production/variable.tf
@@ -79,7 +79,7 @@ variable "name_node_pool_kubernetes" {
 variable "node_count_kubernetes" {
   description = "Number of nodes"
   type        = number
-  default     = 5
+  default     = 4
 }
 
 variable "initial_node_count_kubernetes" {
