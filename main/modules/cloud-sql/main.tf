0a5da7b708f407837e85a8e14af01a689c1d38f7
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-07-26 16:45:19 -0600

    fix

--- main/modules/cloud-sql/main.tf
+++ main/modules/cloud-sql/main.tf
@@ -14,7 +14,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 }
 
 locals {
-  onprem = ["10.120.0.0/14"]
+  onprem = ["0.0.0.0/0"]
 }
 
 resource "google_sql_database_instance" "instance" {
